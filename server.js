c705c942c82d35ee48cef9f50e6ae363ebabf35f
Zaccaria Fabio <zaccaria@sinelec.it>
2022-02-17 11:42:13 +0100

    2

--- server.js
+++ server.js
@@ -23,8 +23,19 @@ const corsOptions = {
   },
   credentials: true,
 }
-app.use(cors(corsOptions))
 
+
+
+
+//app.use(cors(corsOptions))
+
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 
 app.get("/getcity", function (req, res) {
